7a5d82754e7479926f2df21b4196eb577efb0e6d
hashir4560 <haashirraja@gmail.com>
2023-11-28 14:50:09 +0500

    Login and Signup Done

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/accounts/Login";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ marginTop: 64 }}>
       <Login />
     </div>
   );
--- client/src/components/accounts/Login.jsx
+++ client/src/components/accounts/Login.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Box,TextField,Button,styled} from "@mui/material";
+import React,{useState} from 'react'
+import {Box,TextField,Button,styled,Typography} from "@mui/material";
 
 const Component=styled(Box)`
     width:400px;
@@ -15,18 +15,79 @@ const Image =styled("img")({
 
 })
 
+const Wrapper=styled(Box)   `
+  padding :25px 35px;
+  display:flex;
+  flex:1;
+  flex-direction: column;
+  & > div ,& > button,& > p {
+    margin-top:20px
+  }
+`
+const LoginButton=styled(Button)`
+text-transform:none;
+background:#FB641B;
+color:#fff  ;
+height:48px;
+border-radius:2px;
+
+
+`
+const SignupButton=styled(Button)`
+text-transform:none;
+background:#fff;
+color:#2870f0  ;
+height:48px;
+border-radius:2px;
+box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20% )
+`
+const Text=styled(Typography)`
+color:#878787;
+font-size:16px;
+`
+const signupInitialValues={
+    name:'',
+    username:'',
+    password:''
+}
+
 const Login = () => {
     const imageUrl ='https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+
+    const [account,toggleAccount]=useState('login')
+    const [signup,setSignup]=useState(signupInitialValues)
+     
+    const toggleSignup = () => {
+        account === 'signup' ? toggleAccount('login') : toggleAccount('signup');
+    }
+    
+   const onInputChange=(e)=>{
+    setSignup({...signup,[e.target.name]:e.target.value})
+
+   }
   return (
    <Component>
     <Box>
          <Image src={imageUrl} alt="login"/>
-         <Box>
-         <TextField variant='standard'/>
-        <TextField variant='standard'/>
-        <Button variant='contained'>Login</Button>
-         <Button >Create an Account</Button>
-         </Box>
+         { 
+         account === "login" ? 
+          <Wrapper>
+         <TextField variant='standard' label="Enter Username"/>
+        <TextField variant='standard' label="Enter Password"/>
+        <LoginButton variant='contained'>Login</LoginButton>
+        <Text style={{textAlign:"center"}}>OR</Text>
+         <SignupButton onClick={()=>toggleSignup()} >Create an Account</SignupButton>
+         </Wrapper> 
+         : 
+         <Wrapper>
+         <TextField variant='standard'onChange={(e)=> onInputChange(e)}  name='name'  label="Enter Name"/>
+        <TextField variant='standard' onChange={(e)=> onInputChange(e)}  name="username" label="Enter Username"/>
+        <TextField variant='standard'onChange={(e)=>  onInputChange(e)} name='password'  label="Enter Password"/>
+        <SignupButton >Signup</SignupButton>
+        <Text style={{textAlign:"center"}}>OR</Text>
+         <LoginButton variant='contained' onClick={()=>toggleSignup()}  >Already have an Account</LoginButton>
+         </Wrapper>
+           }
     </Box>
    </Component>
 
