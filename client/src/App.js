c331373302897954830fc775e154cc1639e28ce4
hashir4560 <haashirraja@gmail.com>
2023-11-28 12:17:20 +0500

    Created Project With React and made Login Page

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+//components
+import Login from "./components/accounts/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+export default App;
