761f48b6749f38e5eed586455b427a4059427293
hashir4560 <haashirraja@gmail.com>
2023-12-04 13:34:55 +0500

    Added Header Component

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //components
 import Login from "./components/accounts/Login";
 import Home from "./components/home/Home";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <DataProvider>
       <BrowserRouter>
+        <Header />
         <div style={{ marginTop: 64 }}>
           <Routes>
             <Route path="/login" element={<Login />} />
