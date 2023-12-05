61c0cc14ffdf985c22f74dc9a053f8d5676f832b
hashir4560 <haashirraja@gmail.com>
2023-12-05 18:15:54 +0500

    Updated Create Post

--- client/src/components/create/CreatePost.jsx
+++ client/src/components/create/CreatePost.jsx
@@ -1,3 +1,5 @@
+import { useState,useEffect } from "react"
+
 import { Box,styled ,FormControl,InputBase, Button, TextareaAutosize} from "@mui/material"
 import {AddCircle as Add} from '@mui/icons-material'
 
@@ -35,10 +37,47 @@ const Textarea=styled(TextareaAutosize)`
         outline:none
     }
 `
+const initialPost={
+    title:'',
+    description:'',
+    picture:'',
+    username:'',
+    categories:'',
+    createdDate: new Date()
+
+}
 
 
 const CreatePost=()=>{
     const url ='https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
+    
+    const [post,setPost]=useState(initialPost);
+    
+    const [file,setFile]=useState('');
+    useEffect(()=>{
+        const getImage=()=>{
+            if(file){
+                const data=new FormData();
+                data.append("name",file.name);
+                data.append("file",file) ;  
+
+
+                //API CALL 
+                post.picture=''      
+            }
+        }
+
+        getImage();
+        post.categories=''
+
+
+    },[file])
+  
+    const handleChange=(e)=>{
+        setPost({ ...post,[e.target.name]:[e.target.value]})
+    }
+
+
     return(
         <Container>
             <Image src={url} alt='banner'/>
@@ -50,23 +89,22 @@ const CreatePost=()=>{
                 <input type="file"
                 id="fileInput"   
                  style={{display:'none'}}
+                 onChange={(e)=> setFile(e)}
             
 
                 />
 
-                <InputTextField placeholder='Title' />
+                <InputTextField placeholder='Title' onChange={(e)=> handleChange(e)} name="title" />
                 <Button variant="contained">Publish</Button>
 
             </StyledFormControl>
             <Textarea
               minRows={5}
               placeholder="Tell Your Story "
+              onChange={(e)=> handleChange(e.target.files[0])} name="description"
             
 
             />
-
-
-
         </Container>
     )
 }
