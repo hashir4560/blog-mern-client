93a5a2712844f2420a71ab193e81a4f04b622efd
hashir4560 <haashirraja@gmail.com>
2023-12-06 15:00:32 +0500

    Made API Call for picture

--- client/src/components/create/CreatePost.jsx
+++ client/src/components/create/CreatePost.jsx
@@ -1,9 +1,14 @@
-import { useState,useEffect } from "react"
+import { useState,useEffect ,useContext} from "react"
+
 
 import { Box,styled ,FormControl,InputBase, Button, TextareaAutosize} from "@mui/material"
 import {AddCircle as Add} from '@mui/icons-material'
 
+import { useLocation } from "react-router-dom"
 
+import { DataContext } from "../../context /DataProvider"
+
+import { API } from "../../service/api"
 
 
 const Container=styled(Box)`
@@ -49,32 +54,43 @@ const initialPost={
 
 
 const CreatePost=()=>{
-    const url ='https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
+   
     
     const [post,setPost]=useState(initialPost);
     
     const [file,setFile]=useState('');
+
+    const {account}=useContext(DataContext)
+
+    const location = useLocation();
+    
+    const url =post.picture? post.picture:'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
+ 
+
     useEffect(()=>{
-        const getImage=()=>{
+        const getImage=async()=>{
             if(file){
-                const data=new FormData();
+                const data= new FormData();
                 data.append("name",file.name);
                 data.append("file",file) ;  
 
 
                 //API CALL 
-                post.picture=''      
+                const response=await API.uploadFile(data)
+                post.picture=response.data;
+                
             }
         }
 
         getImage();
-        post.categories='s'
+        post.categories=location.search?.split('=')[1]||'All';
+        post.username=account.username;
 
 
     },[file])
   
     const handleChange=(e)=>{
-        setPost({ ...post,[e.target.name]:[e.target.value]})
+        setPost({ ...post,[e.target.name]:e.target.value})
     }
 
 
@@ -89,14 +105,14 @@ const CreatePost=()=>{
                 <input type="file"
                 id="fileInput"   
                  style={{display:'none'}}
-                 onChange={(e)=> setFile(e)}
+                 onChange={(e)=> setFile(e.target.files[0])}
             
 
                 />
 
                 <InputTextField placeholder='Title' onChange={(e)=> handleChange(e)} name="title" />
                 <Button variant="contained">Publish</Button>
-
+            
             </StyledFormControl>
             <Textarea
               minRows={5}
